c87cb1fa89a81a7ebc016d42a75784d4e3b1351d
Ty_Hunt <codestack82@gmail.com>
2016-09-24 11:01:08 -0500

    Add new project

---
+++ Long Distance Calls Project/Long Distance Calls by Tyler Hunt/frm_LongDistanceCalls.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_LongDistanceCalls
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_Clear = New System.Windows.Forms.Button()
+        Me.btn_Exit = New System.Windows.Forms.Button()
+        Me.rdo_Daytime = New System.Windows.Forms.RadioButton()
+        Me.grb_CallRates = New System.Windows.Forms.GroupBox()
+        Me.rdo_OffTime = New System.Windows.Forms.RadioButton()
+        Me.rdo_Evening = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn_CalculateCharges = New System.Windows.Forms.Button()
+        Me.grb_Email = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lbl_EnterEmail = New System.Windows.Forms.Label()
+        Me.txt_ReEmail = New System.Windows.Forms.TextBox()
+        Me.txt_Email = New System.Windows.Forms.TextBox()
+        Me.lbl_Output = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.txt_CallMinutes = New System.Windows.Forms.TextBox()
+        Me.grb_CallRates.SuspendLayout()
+        Me.grb_Email.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btn_Clear
+        '
+        Me.btn_Clear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btn_Clear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Clear.Location = New System.Drawing.Point(124, 329)
+        Me.btn_Clear.Name = "btn_Clear"
+        Me.btn_Clear.Size = New System.Drawing.Size(85, 25)
+        Me.btn_Clear.TabIndex = 4
+        Me.btn_Clear.Text = "&Clear"
+        Me.btn_Clear.UseVisualStyleBackColor = True
+        '
+        'btn_Exit
+        '
+        Me.btn_Exit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Exit.Location = New System.Drawing.Point(232, 329)
+        Me.btn_Exit.Name = "btn_Exit"
+        Me.btn_Exit.Size = New System.Drawing.Size(85, 27)
+        Me.btn_Exit.TabIndex = 5
+        Me.btn_Exit.Text = "E&xit"
+        Me.btn_Exit.UseVisualStyleBackColor = True
+        '
+        'rdo_Daytime
+        '
+        Me.rdo_Daytime.AutoSize = True
+        Me.rdo_Daytime.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdo_Daytime.Location = New System.Drawing.Point(23, 28)
+        Me.rdo_Daytime.Name = "rdo_Daytime"
+        Me.rdo_Daytime.Size = New System.Drawing.Size(218, 20)
+        Me.rdo_Daytime.TabIndex = 0
+        Me.rdo_Daytime.Text = "&Daytime Rate  ($0.07 per minute)"
+        Me.rdo_Daytime.UseVisualStyleBackColor = True
+        '
+        'grb_CallRates
+        '
+        Me.grb_CallRates.Controls.Add(Me.rdo_OffTime)
+        Me.grb_CallRates.Controls.Add(Me.rdo_Evening)
+        Me.grb_CallRates.Controls.Add(Me.rdo_Daytime)
+        Me.grb_CallRates.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grb_CallRates.Location = New System.Drawing.Point(8, 12)
+        Me.grb_CallRates.Name = "grb_CallRates"
+        Me.grb_CallRates.Size = New System.Drawing.Size(309, 122)
+        Me.grb_CallRates.TabIndex = 3
+        Me.grb_CallRates.TabStop = False
+        Me.grb_CallRates.Text = "Selcet rate"
+        '
+        'rdo_OffTime
+        '
+        Me.rdo_OffTime.AutoSize = True
+        Me.rdo_OffTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdo_OffTime.Location = New System.Drawing.Point(22, 80)
+        Me.rdo_OffTime.Name = "rdo_OffTime"
+        Me.rdo_OffTime.Size = New System.Drawing.Size(216, 20)
+        Me.rdo_OffTime.TabIndex = 2
+        Me.rdo_OffTime.Text = "&Off-Time Rate ($0.05 per minute)"
+        Me.rdo_OffTime.UseVisualStyleBackColor = True
+        '
+        'rdo_Evening
+        '
+        Me.rdo_Evening.AutoSize = True
+        Me.rdo_Evening.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdo_Evening.Location = New System.Drawing.Point(22, 54)
+        Me.rdo_Evening.Name = "rdo_Evening"
+        Me.rdo_Evening.Size = New System.Drawing.Size(217, 20)
+        Me.rdo_Evening.TabIndex = 1
+        Me.rdo_Evening.Text = "&Evening Rate  ($0.12 per minute)"
+        Me.rdo_Evening.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(37, 157)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Minute&s:"
+        '
+        'btn_CalculateCharges
+        '
+        Me.btn_CalculateCharges.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_CalculateCharges.Location = New System.Drawing.Point(12, 329)
+        Me.btn_CalculateCharges.Name = "btn_CalculateCharges"
+        Me.btn_CalculateCharges.Size = New System.Drawing.Size(85, 25)
+        Me.btn_CalculateCharges.TabIndex = 3
+        Me.btn_CalculateCharges.Text = "C&alculate "
+        Me.btn_CalculateCharges.UseVisualStyleBackColor = True
+        '
+        'grb_Email
+        '
+        Me.grb_Email.Controls.Add(Me.Label2)
+        Me.grb_Email.Controls.Add(Me.lbl_EnterEmail)
+        Me.grb_Email.Controls.Add(Me.txt_ReEmail)
+        Me.grb_Email.Controls.Add(Me.txt_Email)
+        Me.grb_Email.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grb_Email.Location = New System.Drawing.Point(12, 202)
+        Me.grb_Email.Name = "grb_Email"
+        Me.grb_Email.Size = New System.Drawing.Size(309, 86)
+        Me.grb_Email.TabIndex = 1
+        Me.grb_Email.TabStop = False
+        Me.grb_Email.Text = "Enter email info"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "&Re-Enter Email:"
+        '
+        'lbl_EnterEmail
+        '
+        Me.lbl_EnterEmail.AutoSize = True
+        Me.lbl_EnterEmail.Location = New System.Drawing.Point(37, 22)
+        Me.lbl_EnterEmail.Name = "lbl_EnterEmail"
+        Me.lbl_EnterEmail.Size = New System.Drawing.Size(79, 16)
+        Me.lbl_EnterEmail.TabIndex = 1
+        Me.lbl_EnterEmail.Text = "Enter E&mail:"
+        '
+        'txt_ReEmail
+        '
+        Me.txt_ReEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_ReEmail.Location = New System.Drawing.Point(122, 47)
+        Me.txt_ReEmail.Name = "txt_ReEmail"
+        Me.txt_ReEmail.Size = New System.Drawing.Size(178, 22)
+        Me.txt_ReEmail.TabIndex = 2
+        '
+        'txt_Email
+        '
+        Me.txt_Email.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Email.Location = New System.Drawing.Point(122, 17)
+        Me.txt_Email.Name = "txt_Email"
+        Me.txt_Email.Size = New System.Drawing.Size(178, 22)
+        Me.txt_Email.TabIndex = 1
+        '
+        'lbl_Output
+        '
+        Me.lbl_Output.AutoSize = True
+        Me.lbl_Output.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_Output.Location = New System.Drawing.Point(12, 367)
+        Me.lbl_Output.Name = "lbl_Output"
+        Me.lbl_Output.Size = New System.Drawing.Size(67, 16)
+        Me.lbl_Output.TabIndex = 6
+        Me.lbl_Output.Text = "lbl_Output"
+        '
+        'txt_CallMinutes
+        '
+        Me.txt_CallMinutes.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_CallMinutes.Location = New System.Drawing.Point(134, 154)
+        Me.txt_CallMinutes.Name = "txt_CallMinutes"
+        Me.txt_CallMinutes.Size = New System.Drawing.Size(178, 22)
+        Me.txt_CallMinutes.TabIndex = 0
+        '
+        'frm_LongDistanceCalls
+        '
+        Me.AcceptButton = Me.btn_CalculateCharges
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btn_Clear
+        Me.ClientSize = New System.Drawing.Size(327, 361)
+        Me.Controls.Add(Me.lbl_Output)
+        Me.Controls.Add(Me.grb_Email)
+        Me.Controls.Add(Me.btn_CalculateCharges)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_CallMinutes)
+        Me.Controls.Add(Me.grb_CallRates)
+        Me.Controls.Add(Me.btn_Exit)
+        Me.Controls.Add(Me.btn_Clear)
+        Me.Name = "frm_LongDistanceCalls"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Long Distance Calls By Tyler Hunt"
+        Me.grb_CallRates.ResumeLayout(False)
+        Me.grb_CallRates.PerformLayout()
+        Me.grb_Email.ResumeLayout(False)
+        Me.grb_Email.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_Clear As Button
+    Friend WithEvents btn_Exit As Button
+    Friend WithEvents rdo_Daytime As RadioButton
+    Friend WithEvents grb_CallRates As GroupBox
+    Friend WithEvents rdo_OffTime As RadioButton
+    Friend WithEvents rdo_Evening As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btn_CalculateCharges As Button
+    Friend WithEvents grb_Email As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lbl_EnterEmail As Label
+    Friend WithEvents txt_ReEmail As TextBox
+    Friend WithEvents txt_Email As TextBox
+    Friend WithEvents lbl_Output As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents txt_CallMinutes As TextBox
+End Class
---
+++ Long Distance Calls Project/Long Distance Calls by Tyler Hunt/frm_LongDistanceCalls.vb
@@ -0,0 +1,326 @@
+﻿Public Class frm_LongDistanceCalls
+    Private Sub frm_LongDistanceCalls_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Initial load settings
+        rdo_Daytime.Checked = True
+        lbl_Output.Text = String.Empty
+        lbl_EnterEmail.TextAlign = ContentAlignment.MiddleCenter
+        txt_CallMinutes.Focus()
+
+    End Sub
+
+    Private Sub btn_CalculateCharges_Click(sender As Object, e As EventArgs) Handles btn_CalculateCharges.Click
+
+        'Declare and set email vars
+        Dim email1 As String = String.Empty
+        Dim email2 As String = String.Empty
+
+        'Declare & validate email input
+        Dim email_IsValid As Boolean = False
+        Dim vaild_email As String = String.Empty
+
+        'Declare & validate minute inputs
+        Dim min_IsValid As Boolean
+        Dim valid_Minutes As Integer
+
+        'Declare & get selected text 
+        Dim getRateText As String = getSelectedRateText()
+
+        'Declare & get cost per minute and calc total
+        Dim costPerMinute As Decimal = getSelectedCost()
+        Dim totalCost As Decimal = 0D
+
+        'Declare & validate is text inputs are valid
+        Dim text_isValid As Boolean = isTextBoxValid()
+
+        'Ddeclare string for the output label 
+        Dim outputString As String = String.Empty
+
+        'Test conditions and report back...
+        If text_isValid Then
+            'Set values
+            min_IsValid = isMinutesValid()
+
+            email1 = txt_Email.Text
+            email2 = txt_ReEmail.Text
+            email_IsValid = isEmailValid(email1, email2)
+
+            If min_IsValid Then
+                valid_Minutes = getMinutes()
+                If email_IsValid Then
+                    vaild_email = getEmail()
+
+                    'Calculate total Cost
+                    totalCost = costPerMinute * valid_Minutes
+
+                    'Declare output label string and set
+                    outputString = "Category: " & getRateText & vbNewLine &
+                                        "Total minutes: " & valid_Minutes.ToString() & vbNewLine &
+                                        "Total Cost: " & totalCost.ToString("C") & vbNewLine &
+                                        "The bill will be sent to: " & vaild_email
+
+                    'Set output string into the output label
+                    lbl_Output.Text = outputString
+
+                    'Clear form of data for new input
+                    rdo_Daytime.Checked = True
+                    txt_CallMinutes.Text = String.Empty
+                    txt_Email.Text = String.Empty
+                    txt_ReEmail.Text = String.Empty
+
+                    'Resize form to show output label
+                    Me.Size = New System.Drawing.Size(343, 500)
+                End If
+            End If
+        End If
+
+    End Sub
+
+    Private Sub btn_Clear_Click(sender As Object, e As EventArgs) Handles btn_Clear.Click
+
+        'MessageBox setup vars
+        Dim clr_Message As String = "Are you sure that you want to clear the form?"
+        Dim clr_Title As String = "Clear Form Prompt"
+        Dim clr_Style As MsgBoxStyle = MsgBoxStyle.YesNo Or MsgBoxStyle.DefaultButton2 Or MsgBoxStyle.Critical
+        Dim clr_Response As MsgBoxResult = MsgBox(clr_Message, clr_Style, clr_Title)
+
+        'Confirm user request
+        If clr_Response = MsgBoxResult.Yes Then
+            'Clear & reset form
+            rdo_Daytime.Checked = True
+            txt_CallMinutes.Text = String.Empty
+            txt_Email.Text = String.Empty
+            txt_ReEmail.Text = String.Empty
+            lbl_Output.Text = String.Empty
+            Me.Size = New System.Drawing.Size(343, 400)
+        End If
+
+    End Sub
+
+    Private Sub btn_Exit_Click(sender As Object, e As EventArgs) Handles btn_Exit.Click
+
+        'MessageBox setup vars
+        Dim ext_Message As String = "Are you sure that you want to exit the form?"
+        Dim ext_Title As String = "Exit Form Prompt"
+        Dim ext_Style As MsgBoxStyle = MsgBoxStyle.YesNo Or MsgBoxStyle.DefaultButton2 Or MsgBoxStyle.Question
+        Dim ext_Response As MsgBoxResult = MsgBox(ext_Message, ext_Style, ext_Title)
+
+        'Confirm user request
+        If ext_Response = MsgBoxResult.Yes Then
+            'Close Form
+            Me.Close()
+        End If
+
+    End Sub
+
+    Function isMinutesValid() As Boolean
+
+        'Validate the text input and return a bool value
+        Dim isValid As Boolean = False
+        Const maxMinutes As Integer = 20000
+        Dim minutes As Integer = 0
+        Dim is_minutesValid As Boolean = Integer.TryParse(txt_CallMinutes.Text, minutes)
+        Dim err_message As String = String.Empty
+
+        If is_minutesValid Then
+            If minutes > 0 And minutes <= maxMinutes Then
+                isValid = True
+            Else
+                MsgBox("Error: The minutes input value must be an integer" & vbNewLine & vbNewLine _
+                       & "        The minutes input value must be greater than 0 " & vbNewLine & vbNewLine _
+                       & "        The minutes input value must also be less than 20000 " & vbNewLine & vbNewLine _
+                       & "        Please try again!")
+                txt_CallMinutes.Clear()
+                txt_CallMinutes.Focus()
+            End If
+        Else
+            MsgBox("Error: The minutes input value must be an integer")
+            txt_CallMinutes.Clear()
+            txt_CallMinutes.Focus()
+        End If
+
+        ' MsgBox(isValid)
+
+        Return isValid
+    End Function
+
+    Function getMinutes() As Integer
+
+        Dim minutes As Integer = 0
+        Dim minutes_IsValid As Boolean = isMinutesValid()
+
+        If (minutes_IsValid) Then
+            minutes = CInt(txt_CallMinutes.Text)
+        End If
+
+        Return minutes
+    End Function
+
+    Function isEmailValid(ByVal Email1 As String, ByVal Email2 As String) As Boolean
+        'Return value
+        Dim isInputValid As Boolean = False
+
+        'Counter vars for Email 1
+        Dim count_AT_Email1 As Integer = 0
+        Dim count_dot_Email1 As Integer = 0
+        Dim letterOrDigit_Email1 As Integer = 0
+
+        'Counter vars for Email 2
+        Dim count_AT_Email2 As Integer = 0
+        Dim count_dot_Email2 As Integer = 0
+        Dim letterOrDigit_Email2 As Integer = 0
+
+        'Loop thru email strings - count chars
+        For Each i As Char In Email1
+            If i = "." Then
+                count_AT_Email1 += 1
+            ElseIf i = "@" Then
+                count_dot_Email1 += 1
+            ElseIf Char.IsLetterOrDigit(i) Then
+                letterOrDigit_Email1 += 1
+            End If
+        Next
+
+        'Loop thru email strings - count chars
+        For Each i As Char In Email2
+            If i = "." Then
+                count_AT_Email2 += 1
+            ElseIf i = "@" Then
+                count_dot_Email2 += 1
+            ElseIf Char.IsLetterOrDigit(i) Then
+                letterOrDigit_Email2 += 1
+            End If
+        Next
+
+        'Ensure the string are the same
+        If Email1.ToLower() = Email2.ToLower() Then  'ensure emails match
+            If count_AT_Email1 = 1 And count_AT_Email2 = 1 Then 'ensure that email address has an @
+                If count_dot_Email1 = 1 And count_dot_Email2 = 1 Then 'ensure email has a .
+                    If letterOrDigit_Email1 > 2 And letterOrDigit_Email2 > 2 Then 'ensure that email has a count of at least 3
+                        isInputValid = True
+                    End If
+                End If
+            End If
+        Else
+            MessageBox.Show("The email addresses you entered do not match or are not valid." & vbNewLine _
+                            & vbNewLine & "Please try again!")
+            'txt_Email.Clear()
+            txt_ReEmail.Clear()
+            txt_Email.Focus()
+        End If
+
+        Return isInputValid
+    End Function
+
+    Function getEmail() As String
+
+        Dim emailAddress As String = String.Empty
+        Dim email1 As String = txt_Email.Text
+        Dim email2 As String = txt_ReEmail.Text
+        Dim email_IsValid As Boolean = isEmailValid(email1, email2)
+
+
+        If (email_IsValid) Then
+            emailAddress = txt_Email.Text
+        End If
+
+        Return emailAddress
+    End Function
+
+    Function getSelectedCost() As Decimal
+        'Declare and set vars
+        Const cost_Daytime As Decimal = 0.07D
+        Const cost_Evening As Decimal = 0.12D
+        Const cost_OffPeak As Decimal = 0.05D
+        Dim cost_PerMinute As Decimal
+        Dim cost_DaytimeChecked As Boolean = rdo_Daytime.Checked
+        Dim cost_EveningChecked As Boolean = rdo_Evening.Checked
+
+        If (cost_DaytimeChecked) Then
+            cost_PerMinute = cost_Daytime
+        ElseIf (cost_DaytimeChecked) Then
+            cost_PerMinute = cost_Evening
+        Else
+            cost_PerMinute = cost_OffPeak
+        End If
+
+        Return cost_PerMinute
+    End Function
+
+    Function getSelectedRateText() As String
+
+        'Declare and set vars
+        Dim costLabel As String
+        Dim cost_DaytimeChecked As Boolean = rdo_Daytime.Checked
+        Dim cost_EveningChecked As Boolean = rdo_Evening.Checked
+        Dim cost_OffPeakeChecked As Boolean = rdo_OffTime.Checked
+
+        If (cost_DaytimeChecked) Then
+            costLabel = rdo_Daytime.Text.Substring(1)
+        ElseIf (cost_EveningChecked) Then
+            costLabel = rdo_Evening.Text.Substring(1)
+        Else
+            costLabel = rdo_OffTime.Text.Substring(1)
+        End If
+
+        Return costLabel
+    End Function
+
+    Function isTextBoxValid() As Boolean
+        Dim isValid = True
+        Dim err_message = String.Empty
+
+        'Test if input values are empty and return correct boolean
+        If txt_CallMinutes.Text = String.Empty Then
+            err_message = "   ~The input minutes text box is empty" & vbNewLine & vbNewLine
+            txt_CallMinutes.Focus()
+            isValid = False
+        End If
+
+        If txt_Email.Text = String.Empty Then
+            err_message += "   ~The email text box is empty" & vbNewLine & vbNewLine
+            txt_Email.Focus()
+            isValid = False
+        End If
+
+        If txt_ReEmail.Text = String.Empty Then
+            err_message += "   ~The confirmation email text box is empty"
+            txt_ReEmail.Focus()
+            isValid = False
+        End If
+
+        'Display error message is
+        If Not err_message = String.Empty Then
+            MsgBox("There is still empty input values in this form:" & vbNewLine & vbNewLine & err_message)
+        End If
+
+
+        Return isValid
+    End Function
+
+    'Event handlers for txt box back color
+    Private Sub txt_CallMinutes_GotFocus(sender As Object, e As EventArgs) Handles txt_CallMinutes.GotFocus
+        Me.txt_CallMinutes.BackColor = Color.LightBlue
+    End Sub
+
+    Private Sub txt_CallMinutes_LostFocus(sender As Object, e As EventArgs) Handles txt_CallMinutes.LostFocus
+        Me.txt_CallMinutes.BackColor = Color.White
+    End Sub
+
+    Private Sub txt_Email_GotFocus(sender As Object, e As EventArgs) Handles txt_Email.GotFocus
+        Me.txt_Email.BackColor = Color.LightBlue
+    End Sub
+
+    Private Sub txt_Email_LostFocus(sender As Object, e As EventArgs) Handles txt_Email.LostFocus
+        Me.txt_Email.BackColor = Color.White
+    End Sub
+
+    Private Sub txt_ReEmail_GotFocus(sender As Object, e As EventArgs) Handles txt_ReEmail.GotFocus
+        Me.txt_ReEmail.BackColor = Color.LightBlue
+    End Sub
+
+    Private Sub txt_ReEmail_LostFocus(sender As Object, e As EventArgs) Handles txt_ReEmail.LostFocus
+        Me.txt_ReEmail.BackColor = Color.White
+    End Sub
+
+End Class
